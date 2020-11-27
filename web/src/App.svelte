cf73d7d8382c462ce8e0af52017c1e3636cb5b20
Guido <guido@q42.nl>
2020-11-27 22:43:42 +0100

    setup up routing

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-	export let name;
+	import { Router, Route } from "svelte-routing";
+	import Home from './routes/Home.svelte';
+	import Case from './routes/Case.svelte';
+
+	export let url = "";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router url="{url}">
+	<Route path="case/:id" component="{Case}" />
+	<Route path="/" component="{Home}" />
+</Router>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
