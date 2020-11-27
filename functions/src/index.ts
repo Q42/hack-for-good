f977b95e6d2a9dd749ce350d05b25a25399f1d0b
Jaap Mengers <jaapm@q42.nl>
2020-11-27 21:53:04 +0100

    Looks up relevant cases for sensor and sends message to all subscribers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,25 +9,44 @@ const { account_sid, auth_token } = functions.config().twilio;
 const client = TwilioClient(account_sid, auth_token);
 
 interface RequestBody {
-  foo: String;
-  bar: String;
+  source: string;
+  sensorId: string;
+}
+
+interface Case {
+  name: string;
+  sensors: string[];
+  subscribers: string[];
 }
 
 export const postEvent = functions.https.onRequest(
   async (request, response) => {
-    // Check for POST request
     if (request.method !== "POST") {
       response.status(400).send("Please send a POST request");
       return;
     }
 
-    const body: RequestBody = JSON.parse(request.body);
+    const body = request.body as RequestBody;
 
-    await db.collection("anomalies").add(body);
-    await client.messages.create({
-      body: `You received an event: ${JSON.stringify(body)}`,
-      from: "+12058983913",
-      to: "+31654237212",
+    const casesRef = await db.collection("cases").get();
+    const cases = casesRef.docs.map((doc) => doc.data() as Case);
+
+    const relevantCases = cases.filter((x) =>
+      x.sensors.includes(body.sensorId)
+    );
+
+    const subscribers = new Set(relevantCases.flatMap((x) => x.subscribers));
+
+    // await db.collection("anomalies").add(body);
+
+    subscribers.forEach(async (subscriber) => {
+      console.log("Sending message to", subscriber);
+
+      await client.messages.create({
+        body: `You received an event: ${JSON.stringify(body)}`,
+        from: "+12058983913",
+        to: subscriber,
+      });
     });
 
     response.send("OK!");
