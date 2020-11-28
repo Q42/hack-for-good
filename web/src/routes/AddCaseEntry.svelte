34c2fe1ae4ae5ceda2d0a1df025bf5374cd7f739
Jaap Mengers <jaapm@q42.nl>
2020-11-28 15:19:24 +0100

    Includes image uplaod button on AddCaseEntry component

--- web/src/routes/AddCaseEntry.svelte
+++ web/src/routes/AddCaseEntry.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
+  import App from "../App.svelte";
+  import UploadImage from "./UploadImage.svelte";
+
   export let caseId = 0;
+
+  let images: string[] = [];
+
+  function imageUploadSucceeded(e: CustomEvent<{ downloadURL: string }>) {
+    images = [...images, e.detail.downloadURL];
+  }
 </script>
-
-<header class="container">
-  <h1>
-    Add case entry
-  </h1>
-</header>
-
-<main class="container">
-  <div class="form-row">
-    <label for="title-input">
-      Title
-    </label>
-    <input id="title-input" type="text">
-  </div>
-</main>
 
 <style>
   .form-row {
@@ -30,3 +24,23 @@
     flex-grow: 1;
   }
 </style>
+
+<header class="container">
+  <h1>Add case entry</h1>
+</header>
+
+<main class="container">
+  <div class="form-row">
+    <label for="title-input"> Title </label>
+    <input id="title-input" type="text" />
+  </div>
+  <div class="form-row">
+    <UploadImage on:imageUploadSucceeded={imageUploadSucceeded} />
+    <ul>
+      {#each images as image}
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <li><img src={image} alt="User uploaded image" /></li>
+      {/each}
+    </ul>
+  </div>
+</main>
