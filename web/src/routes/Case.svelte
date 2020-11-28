1ed6be5f3c965742281ba6d4a38f6f34d8c05a87
Guido <guido@q42.nl>
2020-11-28 09:26:42 +0100

    style case

--- web/src/routes/Case.svelte
+++ web/src/routes/Case.svelte
@@ -2,12 +2,41 @@
   export let id = 0;
 </script>
 
-<h1>
-  Case #{ id }
-</h1>
+<header class="container">
+  <h1>
+    Case #{ id }
+  </h1>
+  <p>
+    Case description
+  </p>
+</header>
+
+<main class="container">
+  <p>
+    Case item 1
+  </p>
+  <p>
+    Case item 2
+  </p>
+</main>
+
+<button aria-label="add case item" class="add-item">
+  +
+</button>
 
 <style>
-  h1 {
-    text-align: center;
+  header {
+    background: #ddd;
+    border-bottom: 1px solid grey;
+    padding-bottom: 5px;
+  }
+
+  button.add-item {
+    position: fixed;
+    left: calc(50% - 30px);
+    bottom: 10px;
+    width: 60px;
+    height: 60px;
+    border-radius: 99px;
   }
 </style>
