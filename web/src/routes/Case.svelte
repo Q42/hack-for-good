d5a23477f6aa36907d05395cbf657df448f4aa8e
Guido <guido@q42.nl>
2020-11-28 14:39:16 +0100

    render case entries

--- web/src/routes/Case.svelte
+++ web/src/routes/Case.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
-  import { Doc } from 'sveltefire';
+  import { Doc, Collection } from 'sveltefire';
   import { Link } from 'svelte-routing';
 
   export let id = 0;
+
+  function readableDate(timestamp) {
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    }
+
+    return new Date(timestamp.seconds).toLocaleString(undefined, options)
+  }
 </script>
 
 <Doc path="cases/{id}" let:data={caseInstance} let:ref={caseRef}>
@@ -25,13 +38,38 @@
       {/each}
     </ul>
 
-    <h2>Case items</h2>
-    <p>
-      Case item 1
-    </p>
-    <p>
-      Case item 2
-    </p>
+    <h2>Case entries</h2>
+    <Collection path={caseRef.collection('entries')} let:data={entries} let:ref={entryRef}>
+      <ul>
+        {#each entries as entry}
+          <li>
+            <p class="timestamp">
+              {readableDate(entry.timestamp)}
+            </p>
+            <h3>
+              {entry.title}
+            </h3>
+            <p>
+              {entry.description}
+            </p>
+
+            <Collection path={entryRef.doc(entry.id).collection('attachments')} let:data={attachments}>
+              {#each attachments as attachment}
+                <div class="attachment">
+                  {#if attachment.type === "photo"}
+                    <figure>
+                      <img src={attachment.url} alt={attachment.alt}>
+                      <figcaption>{attachment.caption}</figcaption>
+                    </figure>
+                  {/if}
+                </div>
+              {/each}
+            </Collection>
+
+          </li>
+        {/each}
+      </ul>
+    </Collection>
   </main>
 </Doc>
 
@@ -44,5 +82,9 @@
     background: #ddd;
     border-bottom: 1px solid grey;
     padding-bottom: 5px;
+  }
+
+  main {
+    padding-bottom: 100px;
   }
 </style>
